54bd1915f1ac570d73bec7492ec9b42536edca9f
Akshay <Akshay02072005@gmail.com>
2026-06-15 19:40:12 +0530

    security: remove config.py from repository and ignore sensitive configuration

--- config.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URI = "mongodb+srv://akshay02072005_db_user:Akshay07123123@deepfake.pmyxjrv.mongodb.net/?appName=Deepfake"
-
-client = MongoClient(MONGO_URI)
-
-db = client["deepfake_db"]
-
-users_collection = db["users"]
